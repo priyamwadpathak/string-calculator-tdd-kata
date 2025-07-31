93e660469d7fac430191422b6766dd6b99d0e9ed
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-07-31 23:28:01 +0530

    GREEN: Parse custom delimiter from input

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,14 @@ class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
         
-        normalized = numbers.gsub("\n", ",")
-        normalized.split(",").map(&:to_i).sum
+        if numbers.start_with?("//")
+            delimiter_line, numbers_part = numbers.split("\n", 2)
+            delimiter = delimiter_line[2..-1]
+            normalized = numbers_part.gsub("\n", delimiter)
+            normalized.split(delimiter).map(&:to_i).sum
+        else
+            normalized = numbers.gsub("\n", ",")
+            normalized.split(",").map(&:to_i).sum
+        end
     end
 end
