858909549e0d7d5ff8ce422ce01b6044fbaba513
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-07-31 23:32:52 +0530

    REFACTOR: Extract delimiter parsing logic into a private method

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,14 +3,20 @@ class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
         
-        if numbers.start_with?("//")
-            delimiter_line, numbers_part = numbers.split("\n", 2)
+        delimiter, numbers_string = parse_delimiter(numbers)
+        normalized = numbers_string.gsub("\n", delimiter)
+        normalized.split(delimiter).map(&:to_i).sum
+    end
+  
+    private
+  
+    def parse_delimiter(input)
+        if input.start_with?("//")
+            delimiter_line, numbers_part = input.split("\n", 2)
             delimiter = delimiter_line[2..-1]
-            normalized = numbers_part.gsub("\n", delimiter)
-            normalized.split(delimiter).map(&:to_i).sum
+            [delimiter, numbers_part]
         else
-            normalized = numbers.gsub("\n", ",")
-            normalized.split(",").map(&:to_i).sum
+            [",", input]
         end
     end
 end
