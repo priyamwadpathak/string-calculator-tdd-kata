f960166383127346aff3e499dc396ddbae89cfcc
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-07-31 23:23:40 +0530

    RED: Add failing test for custom semicolon delimiter

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -31,5 +31,10 @@ RSpec.describe StringCalculator do
         calculator = StringCalculator.new
         expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'handles semicolon delimiter' do
+        calculator = StringCalculator.new
+        expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
